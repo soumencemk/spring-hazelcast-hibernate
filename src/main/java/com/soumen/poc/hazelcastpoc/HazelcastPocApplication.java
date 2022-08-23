4d1c3d4d27afe034ea33c0d69fd1f94eb16e0546
Soumen Karmakar <soumen.karmakar@ericsson.com>
2022-08-23 09:21:23 +0100

    first commit

---
+++ src/main/java/com/soumen/poc/hazelcastpoc/HazelcastPocApplication.java
@@ -0,0 +1,70 @@
+package com.soumen.poc.hazelcastpoc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.List;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class HazelcastPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HazelcastPocApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner(EmployeeRepository repository) {
+        return args -> {
+            Stream.of("Simon", "David", "Ian", "Bob", "Vasilis", "Adam", "Eliza")
+                    .map(name -> new Employee(null, name))
+                    .forEach(repository::save);
+        };
+    }
+}
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    @GetMapping("/employee")
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+}
+
+@Repository
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+class Employee {
+    @Id
+    @GeneratedValue
+    Long id;
+    String name;
+}
